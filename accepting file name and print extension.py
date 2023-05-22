c9e14263be9b122f7e18b903ec0fee8b4a9970c2
tarunika6097 <134293494+tarunika6097@users.noreply.github.com>
2023-05-22 18:57:20 +0530

    Add files via upload

---
+++ accepting file name and print extension.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 22 18:53:47 2023
+
+@author: 30112020
+"""
+
+filename = input("Input the Filename: ")
+f_extns = filename.split(".")
+print ("The extension of the file is : " + repr(f_extns[-1]))
