6134e813cb92a7bd64142b55bdb233d50af377d3
Tarunika <134293494+tarunika6097@users.noreply.github.com>
2023-05-28 17:06:59 +0530

    Add files via upload

---
+++ Program to display the fibonacci sequence.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 28 17:03:28 2023
+
+@author: 30112020
+"""
+
+def recur_fibo(n):
+   if n <= 1:
+       return n
+   else:
+       return(recur_fibo(n-1) + recur_fibo(n-2))
+
+nterms = 14
+
+if nterms <= 0:
+   print("Enter a positive integer")
+else:
+   print("Fibonacci sequence:")
+   for i in range(nterms):
+       print(recur_fibo(i))
